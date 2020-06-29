e89f733da89a5c2d789b95f1d67f9dfb26b21bf5
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-29 19:42:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,6 +10,6 @@ class School
     @roster
 end
 
-school = School.new("Bayside High School")
-school.add_student("Zach Morris", 9) # name and grade
-school.roster
+# school = School.new("Bayside High School")
+# school.add_student("Zach Morris", 9) # name and grade
+# school.roster
