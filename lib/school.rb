6dce273307e5b82cd18b1834ea25f460b5fad969
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-29 19:46:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,6 +13,10 @@ class School
   def add_student(name, grade)
     @name = name
     @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
   end
 end
 
